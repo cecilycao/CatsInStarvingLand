655273fdc90ceb823fa6b3af4826742dac8feae0
cecilycao <caoye1218@gmail.com>
2020-02-11 20:45:31 -0800

    Resources

---
+++ Assets/Scripts/Resources.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resources : MonoBehaviour
+{
+    public enum TileType{
+        EMPTY,
+        DIRT,
+        STONE,
+        SAND,
+        IRON,
+        SPARE
+    }
+}
