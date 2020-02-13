64933566d1e7e0dd4206d5140dc67cc8ffc25b19
cecilycao <caoye1218@gmail.com>
2020-02-13 14:03:33 -0800

    all the picked up items

--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -16,6 +16,41 @@ public class Resources : MonoBehaviour
     public enum PickedUpItemName
     {
         DEFAULT,
-        POOPOO
+        //Mines
+        DIRT,
+        STONE,
+        IRON,
+        SAND,
+        //RESOURCES
+        WOOD,
+        POOPOO,
+        FLOWER,
+        BUTTERFLY,
+        WOOL,
+        SPARE,
+        GAS_TANK,
+        //EATABLE
+        FRUIT,
+        DRIED_FISH,
+        //---TECH STUFF--
+        //WEALPON
+        STONE_CLAW,
+        IRON_CLAW,
+        TECH_CLAW,
+        //STUFF
+        STONE_PICKAXE,
+        IRON_PICKAXE,
+        EXCAVATOR,
+        //LIGHTING
+        LIGHT_BULB,
+        LAMP,
+        LITTLE_SUN,
+        //CLOTH
+        SUMMER_CLOTH,
+        WINTER_CLOTH,
+        //FUNITURE
+        CATERRY,
+        WOOD_FENCE,
+        CHEST
     }
 }
