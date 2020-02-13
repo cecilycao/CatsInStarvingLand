04ec7333032983d8d65ae7b095465a53f6944cc5
cecilycao <caoye1218@gmail.com>
2020-02-13 13:40:16 -0800

    Inventory UI

---
+++ Assets/Scripts/UI/InventorySlot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Text ItemCountText;
+    public Image ItemUIImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void updateItem(Resources.PickedUpItemName ItemName, int count)
+    {
+        ItemCountText.text = count.ToString();
+        //find Image with ItemName
+        ItemUIImage = findImageWithItemName(ItemName);
+    }
+
+    Image findImageWithItemName(Resources.PickedUpItemName ItemName)
+    {
+        
+        //Resources.load();
+        return null;
+    }
+}
