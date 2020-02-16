f30142c4d2fc1157d75f5a12ed2e58cc04abe7bc
Jolly23 <zhaolei@pm.me>
2020-02-16 14:50:00 -0800

    Update PlayerComponent.cs

--- Assets/Scripts/PlayerComponent.cs
+++ Assets/Scripts/PlayerComponent.cs
@@ -26,7 +26,7 @@ public class PlayerComponent : MonoBehaviour
 
     public Transform HoldedPosition;
 
-    private Backpack myBackpack;
+    public Backpack myBackpack;
 
     // Start is called before the first frame update
     void Start()
