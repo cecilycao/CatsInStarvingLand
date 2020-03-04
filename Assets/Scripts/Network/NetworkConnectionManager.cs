80116a6172f22d6163ea53094d00b604953b5dc1
cecilycao <caoye1218@gmail.com>
2020-03-03 19:28:37 -0800

    Photon Network and Animal Assets

---
+++ Assets/Scripts/Network/NetworkConnectionManager.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon.Pun;
+using UnityEngine.UI;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+
+public class NetworkConnectionManager : MonoBehaviourPunCallbacks
+{
+
+    public Button BtnConnectMaster;
+    public Button BtnConnectRoom;
+    public string MainSceneName = "Prototype";
+
+    public bool TriesToConnectToMaster;
+    public bool TriesToConnectToRoom;
+
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        TriesToConnectToMaster = false;
+        TriesToConnectToRoom = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BtnConnectMaster != null)
+            BtnConnectMaster.gameObject.SetActive(!PhotonNetwork.IsConnected && !TriesToConnectToMaster);
+        if (BtnConnectRoom != null)
+            BtnConnectRoom.gameObject.SetActive(PhotonNetwork.IsConnected && !TriesToConnectToMaster && !TriesToConnectToRoom);
+
+    }
+
+    public void OnClickConnectToMaster()
+    {
+        TriesToConnectToMaster = true;
+
+        //Settings (all optional and only for tutorial purpose)
+        PhotonNetwork.OfflineMode = false;           //true would "fake" an online connection
+        PhotonNetwork.NickName = "PlayerName";       //to set a player name
+        PhotonNetwork.AutomaticallySyncScene = true; //to call PhotonNetwork.LoadLevel()
+        PhotonNetwork.GameVersion = "v1";            //only people with the same game version can play together
+
+        //PhotonNetwork.ConnectToMaster(ip,port,appid); //manual connection
+        if (!PhotonNetwork.OfflineMode)
+            PhotonNetwork.ConnectUsingSettings();           //automatic connection based on the config file in Photon/PhotonUnityNetworking/Resources/PhotonServerSettings.asset
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        TriesToConnectToMaster = false;
+        Debug.Log("Connected to Master!");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+        TriesToConnectToMaster = false;
+        TriesToConnectToRoom = false;
+        Debug.Log(cause);
+    }
+
+    public void OnClickConnectToRoom()
+    {
+        if (!PhotonNetwork.IsConnected)
+            return;
+
+        TriesToConnectToRoom = true;
+        //PhotonNetwork.CreateRoom("Peter's Game 1"); //Create a specific Room - Error: OnCreateRoomFailed
+        //PhotonNetwork.JoinRoom("Peter's Game 1");   //Join a specific Room   - Error: OnJoinRoomFailed  
+        PhotonNetwork.JoinRandomRoom();               //Join a random Room     - Error: OnJoinRandomRoomFailed  
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        //no room available
+        //create a room (null as a name means "does not matter")
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 20 });
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        base.OnCreateRoomFailed(returnCode, message);
+        Debug.Log(message);
+        base.OnCreateRoomFailed(returnCode, message);
+        TriesToConnectToRoom = false;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        TriesToConnectToRoom = false;
+
+
+        Debug.Log("Master: " + PhotonNetwork.IsMasterClient + " | Players In Room: " + PhotonNetwork.CurrentRoom.PlayerCount + " | RoomName: " + PhotonNetwork.CurrentRoom.Name + " Region: " + PhotonNetwork.CloudRegion);
+        //if(PhotonNetwork.IsMasterClient && SceneManager.GetActiveScene().name != "Network")
+        //    PhotonNetwork.LoadLevel("Network");
+        if (PhotonNetwork.IsMasterClient && SceneManager.GetActiveScene().name != MainSceneName)
+        {
+            PhotonNetwork.LoadLevel(MainSceneName);
+            //SceneManager.LoadScene(MainSceneName);
+        }
+            
+    }
+}
+
